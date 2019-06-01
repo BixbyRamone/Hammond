acaa52e3d45e3c8bce4bd2b8a1fb51d455bc262f
Gregory T Draheim <GregoryTDraheim@gmail.com>
2019-06-01 16:04:36 -0400

    front end fixes. Auth Repos added

---
+++ Hammond.API/Data/AuthRepository.cs
@@ -0,0 +1,51 @@
+using System.Threading.Tasks;
+using Hammond.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hammond.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await _context.Users.Include(r => r.UserRoles)
+                .SingleOrDefaultAsync(x=> x.UserName == username);
+
+            if (user == null)
+                return null;
+
+            return user;
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }            
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
