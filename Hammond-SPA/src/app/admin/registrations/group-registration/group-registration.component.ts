d36f0d054c0360a21e51f73fbda753de1348d9e6
Gregory T Draheim <GregoryTDraheim@gmail.com>
2019-09-23 22:11:23 -0400

    front-end group registration tweaks

--- Hammond-SPA/src/app/admin/registrations/group-registration/group-registration.component.ts
+++ Hammond-SPA/src/app/admin/registrations/group-registration/group-registration.component.ts
@@ -19,6 +19,10 @@ export class GroupRegistrationComponent implements OnInit {
   pagination: Pagination;
   groupHasMent = false;
   groupHasStud = false;
+  groupToRegister: any = {
+    volunteerIds: [],
+    studentIds: []
+  };
 
   constructor(
     private route: ActivatedRoute,
@@ -48,12 +52,26 @@ export class GroupRegistrationComponent implements OnInit {
     user.grouped = true;
 
     if (user.userRoles[0].role.normalizedName === 'STUDENT') {
+      this.groupToRegister.studentIds.push(user.id);
       this.groupHasStud = true;
     }
     if (user.userRoles[0].role.normalizedName === 'MENTOR') {
+      this.groupToRegister.volunteerIds.push(user.id);
       this.groupHasMent = true;
     }
-    
+  }
+
+  ungroupUser(user: User) {
+    user.grouped = false;
+
+    if (user.userRoles[0].role.normalizedName === 'STUDENT') {
+      const elementToRemove = this.groupToRegister.studentIds.indexOf(user.id);
+      this.groupToRegister.studentIds.splice(elementToRemove, 1);
+    }
+    if (user.userRoles[0].role.normalizedName === 'MENTOR') {
+      const elementToRemove = this.groupToRegister.volunteerIds.indexOf(user.id);
+      this.groupToRegister.volunteerIds.splice(elementToRemove, 1);
+    }
   }
 
   register() {
