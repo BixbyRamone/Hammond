55faf52aa8b7ec9c7c233416f7c2c1b0e589ba6d
BixbyRamone <gregorytdraheim@gmail.com>
2020-01-26 21:25:51 -0500

    changes to run on new laptop

--- Hammond.API/Startup.cs
+++ Hammond.API/Startup.cs
@@ -39,7 +39,7 @@ namespace Hammond.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>
+            services.AddDbContextPool<DataContext>
                 (x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             IdentityBuilder builder = services.AddIdentityCore<User>(opt =>
