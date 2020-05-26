aa9d2a34dc604c5dd2f65dfaa614b527b12f6d8f
BixbyRamone <gregorytdraheim@gmail.com>
2020-05-26 13:06:41 -0400

    ACT catergory scores migration

---
+++ Hammond.API/Migrations/20200526164833_ActTestCats.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hammond.API.Migrations
+{
+    public partial class ActTestCats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EnglishScore",
+                table: "ActScores",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MathmaticsScore",
+                table: "ActScores",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReadingScore",
+                table: "ActScores",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ScienceScore",
+                table: "ActScores",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WritingScore",
+                table: "ActScores",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnglishScore",
+                table: "ActScores");
+
+            migrationBuilder.DropColumn(
+                name: "MathmaticsScore",
+                table: "ActScores");
+
+            migrationBuilder.DropColumn(
+                name: "ReadingScore",
+                table: "ActScores");
+
+            migrationBuilder.DropColumn(
+                name: "ScienceScore",
+                table: "ActScores");
+
+            migrationBuilder.DropColumn(
+                name: "WritingScore",
+                table: "ActScores");
+        }
+    }
+}
