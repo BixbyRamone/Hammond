74c8272aeec7950db3e4366f780216abc7e62a80
BixbyRamone <gregorytdraheim@gmail.com>
2020-05-26 13:07:02 -0400

    ACT category scores added

--- Hammond.API/Models/ActScore.cs
+++ Hammond.API/Models/ActScore.cs
@@ -6,6 +6,11 @@ namespace Hammond.API.Models
     {
         public int Id { get; set; }
         public int Score { get; set; }
+        public int? EnglishScore { get; set; }
+        public int? MathmaticsScore { get; set; }
+        public int? ReadingScore { get; set; }
+        public int? ScienceScore { get; set; }
+        public int? WritingScore { get; set; }
         public DateTime DayOfScore { get; set; }
         public User Student { get; set; }
     }
